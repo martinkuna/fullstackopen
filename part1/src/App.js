665841e73dcf9040fabfb91ea00a76fedbd98da1
Martin Kuna <martinkuna@users.noreply.github.com>
2023-02-01 20:42:52 -0700

    part1a: component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,16 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
+  return (
+    <div>
+      <p>Hello world it is now {now.toString()}</p>
+      <p>
+        {a} + {b} is {a+b}
+      </p>
+    </div>
+  )
+}
 
 export default App
