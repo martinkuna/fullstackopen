133b90e41af5292b46584b4ff22043064d5f18f8
Martin Kuna <martinkuna@users.noreply.github.com>
2023-02-16 19:00:16 -0700

    part1a: complete

--- part1/src/App.js
+++ part1/src/App.js
@@ -7,6 +7,14 @@ const Hello = (props) => {
   )
 }
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/martinkuna'>mk</a>
+    </div>
+  )
+}
+
 const App = () => {
   const name = 'Martin'
   const age = 30
@@ -16,6 +24,7 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name='Austin' age={10+15} />
       <Hello name={name} age={age} />
+      <Footer/>
     </div>
   )
 }
