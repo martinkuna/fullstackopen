a8fa125dab0d22dcd9a45eaaff6ec409f37d3cde
Martin Kuna <martinkuna@users.noreply.github.com>
2023-03-06 20:27:52 -0700

    part1c: event handler

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,15 +3,20 @@ import { useState } from 'react'
 const App = () => {
   const [counter, setCounter] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
+  const handleClick = () => {
+    console.log('clicked')
+  }
 
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        reset
+      </button>
+    </div>
   )
 }
 
