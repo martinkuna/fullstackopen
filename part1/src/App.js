6fdec7b2c9f598592bbf4f92ee6955495a369883
Martin Kuna <martinkuna@users.noreply.github.com>
2023-03-06 19:43:53 -0700

    part1c: page re-render without state

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,34 +1,7 @@
-const Hello = ({ name, age }) => {
-
-  const bornYear = () => new Date().getFullYear() - age
-
+const App = (props) => {
+  const {counter} = props
   return (
-    <div>
-      <p>Hello {name}, you are {age} years old.</p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/martinkuna'>mk</a>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = 'Martin'
-  const age = 30
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name='Austin' age={10+15} />
-      <Hello name={name} age={age} />
-      <Footer/>
-    </div>
+    <div>{counter}</div>
   )
 }
 
