5bc947a1b686ca6621d113d787fc3db6da6181f4
Martin Kuna <martinkuna@users.noreply.github.com>
2023-03-06 20:35:05 -0700

    part1c: passing state to child components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,21 +1,48 @@
 import { useState } from 'react'
 
+/*
+ * One best practice in React is to lift the state up in the component hierarchy.
+ * The documentation says:
+ *   Often, several components need to reflect the same changing data.
+ *   We recommend lifting the shared state up to their closest common ancestor.
+*/
+
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
   
   const increaseByOne = () => setCounter(counter + 1)
-  
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        reset
-      </button>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />  
     </div>
   )
 }
